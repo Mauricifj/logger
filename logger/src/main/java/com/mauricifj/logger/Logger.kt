ad8c3b6196b8dbd310285353f00216dc1bd4d75b
Maurici <m.f.j@hotmail.com>
2020-03-26 13:31:41 -0300

    First commit

---
+++ logger/src/main/java/com/mauricifj/logger/Logger.kt
@@ -0,0 +1,20 @@
+package com.mauricifj.logger
+
+import android.util.Log
+
+object Logger {
+
+    private val TAG = "LOGGER.TAG"
+
+    fun error(message: String, throwable: Throwable? = null) {
+        Log.e(TAG, message, throwable)
+    }
+
+    fun info(message: String) {
+        Log.i(TAG, message)
+    }
+
+    fun warning(message: String) {
+        Log.w(TAG, message)
+    }
+}
